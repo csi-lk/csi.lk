29526f3bd7916e997a36d18b1f151e732ae2a9d9
Callum Silcock <github@csi.lk>
2019-03-19 13:53:06 +0000

    ⚙️ Resolves #49: Removed Google analytics due to privacy concerns

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,75 +1,68 @@
 module.exports = {
   siteMetadata: {
-    title: 'Callum Silcock',
-    siteUrl: 'https://csi.lk',
+    title: "Callum Silcock",
+    siteUrl: "https://csi.lk"
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sass",
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/pages`,
-        name: 'pages',
-      },
+        name: "pages"
+      }
     },
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
         path: `${__dirname}/src/img`,
-        name: 'images',
-      },
+        name: "images"
+      }
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
-            resolve: 'gatsby-remark-prismjs',
+            resolve: "gatsby-remark-prismjs"
           },
           {
-            resolve: 'gatsby-remark-external-links',
+            resolve: "gatsby-remark-external-links",
             options: {
-              target: '_blank',
-              rel: 'nofollow noopener noreferrer',
-            },
-          },
-        ],
-      },
+              target: "_blank",
+              rel: "nofollow noopener noreferrer"
+            }
+          }
+        ]
+      }
     },
-    'gatsby-plugin-catch-links',
+    "gatsby-plugin-catch-links",
     {
-      resolve: 'gatsby-plugin-netlify-cms',
+      resolve: "gatsby-plugin-netlify-cms",
       options: {
-        modulePath: `${__dirname}/src/cms/cms.js`,
-      },
+        modulePath: `${__dirname}/src/cms/cms.js`
+      }
     },
     {
-      resolve: 'gatsby-plugin-nprogress',
+      resolve: "gatsby-plugin-nprogress",
       options: {
         showSpinner: false,
-        color: '#F39C12',
-      },
+        color: "#F39C12"
+      }
     },
+    "gatsby-plugin-sitemap",
     {
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: 'UA-114684520-1',
-        head: false,
-      },
-    },
-    'gatsby-plugin-sitemap',
-    {
-      resolve: 'gatsby-plugin-netlify',
+      resolve: "gatsby-plugin-netlify",
       options: {
         headers: {
-          '/*': [
-            'Strict-Transport-Security: max-age=31536000; includeSubDomains; preload',
-          ],
-        },
-      },
-    },
-  ],
-}
+          "/*": [
+            "Strict-Transport-Security: max-age=31536000; includeSubDomains; preload"
+          ]
+        }
+      }
+    }
+  ]
+};
