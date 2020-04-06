4c834992b1940c4fbe453468209177f6b74b9ab1
Callum Silcock <github@csi.lk>
2020-04-06 19:09:44 +0100

    feat(header): created global header component

--- src/components/header.ts
+++ src/components/header.ts
@@ -1,13 +1,22 @@
-export = function Header(): string {
-  return `
-<!doctype html>
-<html lang="en" class="no-js">
-<head>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta name="description" content="{{ renderData.description or description or metadata.description }}">
-  <title>{{ renderData.title or title or metadata.title }}</title>
-  <link rel="stylesheet" href="/styles/index.css">
-</head>
-`
+import { html } from 'common-tags'
+import { Eleventy } from '../types/global'
+
+export = function Header({
+  siteData: { meta, title: siteTitle, description: siteDescription },
+  title,
+  description,
+}: Eleventy.Page): string {
+  const metaTags = meta.map(
+    tag => `<meta ${Object.keys(tag).map(key => `${key}="${tag[key]}"`)} />`,
+  )
+  return html`<!DOCTYPE html>
+    <html lang="en" class="no-js">
+      <head>
+        ${metaTags}
+        <meta name="description" content="${description || siteDescription}" />
+        <title>${title || siteTitle}</title>
+        <link rel="stylesheet" href="/styles/index.css" />
+      </head>
+      <body></body>
+    </html> `
 }
