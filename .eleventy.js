c148dd0c4a2bc6c2f8f8f820607591011f56398d
Callum Silcock <github@csi.lk>
2020-04-07 18:28:24 +0100

    fix(eleventy): removed header, build fails without it

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,6 @@
 const componentsDir = './src/_includes/components'
-const Header = require(`${componentsDir}/header.js`)
 
 module.exports = function (config) {
-  config.addShortcode('Header', Header)
   config.setUseGitIgnore(false)
   return {
     dir: { input: 'src', output: 'dist', data: '_data' },
