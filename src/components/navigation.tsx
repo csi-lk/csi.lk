6cd3907d62a249bd9ac06a1c115f293f7a286577
Callum Silcock <github@csi.lk>
2020-04-07 16:04:32 +0100

    feat(navigation): created navigation component

---
+++ src/components/navigation.tsx
@@ -0,0 +1,10 @@
+const Navigation = () => (
+  <nav>
+    <a className="logo" href="/">
+      <i className="terminal"></i>
+      <h2 className="logo">Callum{`\n`}Silcock</h2>
+    </a>
+  </nav>
+)
+
+export default Navigation
