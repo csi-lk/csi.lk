2628c42cd43769c79fd98493ac1d918c1a9e4b0c
Callum Silcock <github@csi.lk>
2020-04-08 19:59:47 +0100

    feat(navigation): increased spacing and added articles link

--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -1,11 +1,12 @@
 import * as Silk from '../../lib/silk'
 
 const Navigation = (): HTMLElement => (
-  <nav>
+  <nav className="navigation">
     <a className="logo" href="/">
       <i className="icon-terminal"></i>
       <h2 className="logo">Callum{`\n`}Silcock</h2>
     </a>
+    <a href="articles.html">articles</a>
   </nav>
 )
 
