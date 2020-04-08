187c85c8e08e4d5137929590bb66a00a2fde5a51
Callum Silcock <github@csi.lk>
2020-04-08 19:31:24 +0100

    feat(home): use new keywords meta

--- src/layouts/home/home.tsx
+++ src/layouts/home/home.tsx
@@ -2,12 +2,9 @@ import * as Silk from '../../lib/silk'
 import PageContainer from '../../components/page-container/page-container'
 import Eleventy from '../../types/global'
 
-export = function Home({
-  content,
-  'site-data': { meta, title, description },
-}: Eleventy.Page): string {
+export = function Home({ content, 'site-data': siteData }: Eleventy.Page): string {
   return (
-    <PageContainer meta={meta} title={title} description={description} style="fixed">
+    <PageContainer {...siteData} style="fixed">
       <article id="content">{content}</article>
     </PageContainer>
   )
