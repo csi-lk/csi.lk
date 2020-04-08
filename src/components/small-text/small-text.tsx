1b8cf7f0898aed4dcc500746986265dc0f0a676d
Callum Silcock <github@csi.lk>
2020-04-08 15:24:43 +0100

    fix(small-text): should be p instead of span

--- src/components/small-text/small-text.tsx
+++ src/components/small-text/small-text.tsx
@@ -1,5 +1,5 @@
 import * as Silk from '../../lib/silk'
 
-const SmallText = (content: string): HTMLElement => <span className="small-text">{content}</span>
+const SmallText = (content: string): HTMLElement => <p className="small-text">{content}</p>
 
 export default SmallText
