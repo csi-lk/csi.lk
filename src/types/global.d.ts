355238fa6eaf6e485e40fc03c41f595f74ee947f
Callum Silcock <github@csi.lk>
2020-04-08 18:22:25 +0100

    build: infer the article types from logging out 11ty data

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,3 +1,15 @@
+export interface Article {
+  data: {
+    title: string
+    description?: string
+    path: string
+    template: string
+    meta?: string
+    permalink: string
+  }
+  date: Date
+}
+
 declare namespace Eleventy {
   interface Page {
     'site-data': typeof import('../_data/site-data.json')
@@ -5,6 +17,10 @@ declare namespace Eleventy {
     title?: string
     description?: string
     content: string
+    collections: {
+      all: Article[]
+      article: Article[]
+    }
   }
 }
 
