d095e991785cde4fe54fe1532f7b2ee2866dae04
Callum Silcock <github@csi.lk>
2020-04-08 15:05:23 +0100

    build: programatically make all components available as shortcodes in markdown

--- .eleventy.js
+++ .eleventy.js
@@ -1,20 +1,32 @@
+const fs = require('fs')
 const md = require('markdown-it')
 const mdExternalLinks = require('markdown-it-external-links')
+
 const componentsDir = './src/_includes/components'
-const BigText = require(`${componentsDir}/big-text/big-text`)
+const toPascalCase = s => s.replace(/(^\w|-\w)/g, t => t.replace(/-/, '').toUpperCase())
 
 module.exports = function (config) {
+  // Markdown plugins
   const markdownLib = md({ html: true }).use(mdExternalLinks, {
     externalClassName: 'external',
     externalTarget: '_blank',
     externalRel: 'noopener',
     internalDomains: ['csi.lk', 'callumsilcock.com'],
   })
-
-  config.setUseGitIgnore(false)
-  config.addPairedShortcode('BigText', BigText.default)
   config.setLibrary('md', markdownLib)
 
+  // Use custom ignore
+  config.setUseGitIgnore(false)
+
+  // Add all components as {% shortcodes %}
+  fs.readdirSync(componentsDir).forEach(folder => {
+    config.addPairedShortcode(
+      toPascalCase(folder),
+      require(`${componentsDir}/${folder}/${folder}.js`).default,
+    )
+  })
+
+  // Config
   return {
     dir: { input: 'src', output: 'dist', data: '_data' },
     passthroughFileCopy: true,
