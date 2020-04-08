f707e014b37fb65233c83e4692792b925236d047
Callum Silcock <github@csi.lk>
2020-04-08 12:15:49 +0100

    feat: external links now have classname and open blank by default, changed working on terminal css icon

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,20 @@
+const md = require('markdown-it')
+const mdExternalLinks = require('markdown-it-external-links')
 const componentsDir = './src/_includes/components'
 const BigText = require(`${componentsDir}/big-text/big-text`)
 
 module.exports = function (config) {
+  const markdownLib = md({ html: true }).use(mdExternalLinks, {
+    externalClassName: 'external',
+    externalTarget: '_blank',
+    externalRel: 'noopener',
+    internalDomains: ['csi.lk', 'callumsilcock.com'],
+  })
+
   config.setUseGitIgnore(false)
   config.addPairedShortcode('BigText', BigText.default)
+  config.setLibrary('md', markdownLib)
+
   return {
     dir: { input: 'src', output: 'dist', data: '_data' },
     passthroughFileCopy: true,
--- src/components/navigation/navigation.tsx
+++ src/components/navigation/navigation.tsx
@@ -3,7 +3,7 @@ import * as Silk from '../../lib/silk'
 const Navigation = (): HTMLElement => (
   <nav>
     <a className="logo" href="/">
-      <i className="terminal"></i>
+      <i className="icon-terminal"></i>
       <h2 className="logo">Callum{`\n`}Silcock</h2>
     </a>
   </nav>
