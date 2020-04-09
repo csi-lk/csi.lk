084b50976d694b4004bd6c95f7bab069ac64daa4
Callum Silcock <github@csi.lk>
2020-04-09 13:15:52 +0100

    feat(article-list): show post date for each article and sort them by date

--- src/layouts/article-list/article-list.tsx
+++ src/layouts/article-list/article-list.tsx
@@ -1,11 +1,11 @@
 import * as Silk from '../../lib/silk'
 import PageContainer from '../../components/page-container/page-container'
+import Time from '../../components/time/time'
 import Eleventy from '../../types/global'
 
 export = function ArticleList({
   content,
   'site-data': { meta, title: siteTitle, description: siteDescription, keywords: siteKeywords },
-
   collections: { article: articles },
 }: Eleventy.Page): string {
   return (
@@ -18,10 +18,13 @@ export = function ArticleList({
       <article id="content">{content}</article>
       <div className="article-list">
         {articles
-          .map(({ data: { permalink, title, keywords, description } }) => (
+          .reverse()
+          .map(({ data: { permalink, title, keywords, description }, date }) => (
             <a href={permalink}>
               <h3>{title}</h3>
-              {keywords && <h6>{keywords}</h6>}
+              <h6>
+                <Time date={date} /> - {keywords && keywords}
+              </h6>
               <p>{description}</p>
             </a>
           ))
