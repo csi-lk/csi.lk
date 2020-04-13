7ce47e2d89cfd794e2b235dd7c34c135de2110c3
Callum Silcock <github@csi.lk>
2020-04-13 12:08:16 +0100

    test(footer): add tests for fixed class prop

---
+++ src/components/footer/footer.test.tsx
@@ -0,0 +1,10 @@
+import { getByTestId } from '@testing-library/dom'
+import Silk from '../../lib/silk'
+import Footer from './footer'
+
+describe('footer', () => {
+  it('renders classname if passed as a prop', () => {
+    const wrapper = mount(<Footer fixed />)
+    expect(getByTestId(wrapper, 'footer')).toHaveClass('fixed')
+  })
+})
