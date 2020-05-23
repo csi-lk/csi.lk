0d10b7bc580c4573e41b7300e19f6168f3c3cd66
Callum Silcock <github@csi.lk>
2020-05-23 18:21:50 +0100

    style(small-text): type unused var to suppress warning

--- src/components/small-text/small-text.tsx
+++ src/components/small-text/small-text.tsx
@@ -1,5 +1,7 @@
 import * as Silk from '../../lib/silk'
 
-const SmallText = (_, content: string): HTMLElement => <p className="small-text">{content}</p>
+const SmallText = (_: unknown, content: string): HTMLElement => (
+  <p className="small-text">{content}</p>
+)
 
 export default SmallText
