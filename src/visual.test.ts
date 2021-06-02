bf144b0dc10139b49bb11040c8c93746a0ba79d8
Callum Silcock <github@csi.lk>
2021-06-02 15:03:13 +1000

    ci: add percy test to master, pr steps
    as well as simplify cache in master

--- src/visual.test.ts
+++ src/visual.test.ts
@@ -1,18 +1,22 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import PercyScript from '@percy/script'
 
+const percyOptions = { widths: [768] }
+
 PercyScript.run(
   async (
     page: { goto: (url: string) => Promise<void> },
-    percySnapshot: (snapshotName: string) => Promise<void>,
+    percySnapshot: (snapshotName: string, options: { widths: number[] }) => Promise<void>,
   ) => {
     await page.goto('http://localhost:8080/')
-    await percySnapshot('home')
+    await percySnapshot('home', percyOptions)
     await page.goto('http://localhost:8080/articles.html')
-    await percySnapshot('articles')
+    await percySnapshot('articles', percyOptions)
+    await page.goto('http://localhost:8080/timeline.html')
+    await percySnapshot('timeline', percyOptions)
     await page.goto('http://localhost:8080/articles/my-macos-shortcuts')
-    await percySnapshot('my-macos-shortcuts')
+    await percySnapshot('my-macos-shortcuts', percyOptions)
     await page.goto('http://localhost:8080/404')
-    await percySnapshot('404')
+    await percySnapshot('404', percyOptions)
   },
 )
