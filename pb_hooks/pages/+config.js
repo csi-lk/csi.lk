36bab4d0dc2a5dc15b53ba04e0b4d8b943bcae9f
fspec <fspec@fspec.dev>
2025-12-18 16:00:22 +1100

    fix: add js-sdk plugin dependency
    Load both js-sdk and auth plugins since auth depends on js-sdk.

--- pb_hooks/pages/+config.js
+++ pb_hooks/pages/+config.js
@@ -5,10 +5,12 @@
  * Load auth plugin via require() from deployed node_modules
  */
 
+const jsSdkPlugin = require('pocketpages-plugin-js-sdk');
 const authPlugin = require('pocketpages-plugin-auth');
 
 module.exports = {
   plugins: [
+    jsSdkPlugin,
     authPlugin
   ],
   debug: false
